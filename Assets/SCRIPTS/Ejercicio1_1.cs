bdd28c17c715378cecf5194925d0b7b5db944423
adelamarinmr <adelamarinmr@gmail.com>
2024-09-11 19:59:55 +0200

    apuntes

--- Assets/SCRIPTS/Ejercicio1_1.cs
+++ Assets/SCRIPTS/Ejercicio1_1.cs
@@ -4,10 +4,27 @@ using UnityEngine;
 
 public class Ejercicio1_1 : MonoBehaviour
 {
+    //Declarar variables
+    //int vida; 
+    //float porcentajeAcierto;
+    //bool quemado; 
+    //string explorador;
+    //char solucionPuzzle;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        //vida = 8; //Aignar un valor a la variable.
+        //porcentajeAcierto = 6.8f;
+        //quemado = false;
+        //explorador = "Dani Colado";
+        //solucionPuzzle = 'R';
+
         
+
+
+
     }
 
     // Update is called once per frame
