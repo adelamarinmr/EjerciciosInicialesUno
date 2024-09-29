bfc8c935c56b2fd9a0b10e50e7a754e519fecfc9
adelamarinmr <adelamarinmr@gmail.com>
2024-09-29 18:29:46 +0200

    más2

---
+++ Assets/SCRIPTS/Ej6/Ej6_1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej6_1 : MonoBehaviour
+{
+
+    // a) true
+    // b) true
+    // c) true
+    // d) false
+    // e) true
+    // f) true
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Assets/SCRIPTS/Ej6/Ej6_2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej6_2 : MonoBehaviour
+{
+
+    // a)false
+    // b)true
+    // c)true
+    // d)true
+    // e)true
+    // f)false
+
+
+
+
+
+
+}
